800c7b3f35d2b7e915cc51b1654b15d6eb1d7fda
Faiz Rahmadani <fzrahmadan@gmail.com>
2022-11-13 10:05:27 +0700

    minor change

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -14,6 +14,9 @@
     <meta name="robots" content="index,follow" />
     {{-- CSS --}}
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
     <link rel="shortcut icon" href="{{ asset('img/epw-favicon.png') }}" type="image/x-icon">
     {{-- Icons --}}
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
@@ -26,7 +29,8 @@
 <body>
 
 
-    @yield('content')
+    @yield('content')'
+
 
 
 
