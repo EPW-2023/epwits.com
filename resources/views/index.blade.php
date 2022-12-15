f77cb725afd10757e0820ff2d696f534c3f83381
Faiz Rahmadani <fzrahmadan@gmail.com>
2022-12-15 18:16:36 +0700

    update epw main page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,10 +1,10 @@
 @extends('layout.layout')
 @section('content')
     <div class="bg-homepage-1">
-        <img class="batik-homepage-1" src="{{ asset('img/batik-homepage-1.svg') }}" alt="batik mega mendung lazu">
+        <img class="batik-homepage-1" src="{{ asset('img/batik-homepage-1.png') }}" alt="batik mega mendung lazu">
         <img class="pita-homepage-2" src="{{ asset('img/pita-homepage-1.svg') }}" alt="wave">
         @include('partials.navbar')
-        <div class="container hero-section mt-5 d-flex justify-content-center align-items-center">
+        <div class="container hero-section mt-5 d-flex justify-content-center flex-wrap-reverse align-items-center">
             <div>
                 <div class="welcometo-text">Welcome to</div>
                 <div class="big-text-epw">
@@ -14,7 +14,7 @@
                 <div class="year-2023">2023</div>
             </div>
             <div>
-                <img src="{{ asset('img/city-homepage.svg') }}" alt="city future mantap jiwa" width="500">
+                <img src="{{ asset('img/city-homepage.png') }}" class="future-city" alt="city future mantap jiwa">
             </div>
         </div>
         <div class="d-flex justify-content-center mt-3">
@@ -22,12 +22,12 @@
                     class="fa-solid fa-arrow-down"></i></a>
         </div>
         <div id="theme-section" class="pt-5">
-            <div class="mx-auto first-theme d-flex justify-content-start">
+            <div class="mx-auto first-theme">
                 <div class="text-center d-flex align-items-start">
                     <span class="the me-2">“The</span> Implementation <span class="the mx-2">Of</span> Technology
                 </div>
             </div>
-            <div class="mx-auto second-theme d-flex justify-content-end">
+            <div class="mx-auto second-theme">
                 <div class="text-center d-flex align-items-start">
                     <span class="towards me-2">towards</span> Cultural Sustainability"
                 </div>
@@ -46,11 +46,11 @@
         {{-- End of EPW Adalah --}}
         {{-- What inside  --}}
         <img class="line-element" src="{{ asset('img/line-element.png') }}" alt="wave line element ">
-        <div id="what-inside-section">
+        <div class="" id="what-inside-section">
             <h2 class="header text-center">
                 What's Inside?
             </h2>
-            <div class="container body d-flex justify-content-center">
+            <div class="container body d-flex justify-content-center flex-wrap">
                 <a href="#" class="text-decoration-none">
                     <div class="foto-box foto-tf">
                         <div class="inner-text">
@@ -84,6 +84,10 @@
                     </div>
                 </a>
             </div>
+        </div>
+        {{-- Responsive What Inside --}}
+        <div class="" id="what-inside-section-responsive">
+
         </div>
         {{-- End of What inside --}}
         <div id="event-timeline">
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -2,25 +2,26 @@
     <div class="container-fluid">
         <img src="{{ asset('img/Logo EPW.png') }}" width="60" alt="">
         <a class="navbar-brand ms-4" href="#">EPW 2023</a>
-        <button type="button" class="btn btn-primary bg-transparent border-0" id="button-modal" data-bs-toggle="modal"
-            data-bs-target="#exampleModal">
-            <i class="bi bi-list fs-3"></i>
+        <button type="button" class="btn btn-primary  bg-transparent border-0" id="button-modal" type="button"
+            data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+            aria-expanded="false" aria-label="Toggle navigation">
+            <i class="fa-solid fa-bars fs-3"></i>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                <li class="nav-item">
+                <li class="nav-item text-center">
                     <a class="nav-link nav-link-epw" id="nav-link-epw" href="https://epwits.org">Home</a>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item text-center">
                     <a class="nav-link nav-link-epw" id="nav-link-epw" href="#">About</a>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item text-center">
                     <a class="nav-link nav-link-epw" id="nav-link-epw" href="https://epc.epwits.org">EPC</a>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item text-center">
                     <a class="nav-link nav-link-epw" id="nav-link-epw" href="https://injection.epwits.org">INJECTION</a>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item text-center">
                     <a class="nav-link nav-link-epw" id="nav-link-epw" href="#">MERCH</a>
                 </li>
             </ul>
